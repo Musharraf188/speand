f0feb54eb2100d4697b13fd2f1b9e7000832f390
Musharraf188 <mr.musharraf188@gmail.com>
2023-06-26 23:27:48 +0530

    ExpenseDate/Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App(){
+  const expenses=[
+    {
+      id:1,
+      title:'Car Insurance',
+      amount:243.98,
+      date:new Date(2020, 6, 12)
+    },
+    {
+      id:1,
+      title:'Car Insurance',
+      amount:243.98,
+      date:new Date(2020, 6, 12)
+    },
+    {
+      id:1,
+      title:'Car Insurance',
+      amount:243.98,
+      date:new Date(2020, 6, 12)
+    },
+    {
+      id:1,
+      title:'Car Insurance',
+      amount:243.98,
+      date:new Date(2020, 6, 12)
+    },
+    
+
+  ]
   return <> <p>Hello World..!</p>
-  <ExpenseItem /></>
+  <ExpenseItem 
+  title={expenses[0].title} 
+  amount={expenses[0].amount} 
+  date={expenses[0].date}
+  />
+  <ExpenseItem 
+  title={expenses[1].title} 
+  amount={expenses[1].amount} 
+  date={expenses[1].date}
+  />
+  <ExpenseItem 
+  title={expenses[2].title} 
+  amount={expenses[2].amount} 
+  date={expenses[2].date}
+  />
+  <ExpenseItem 
+  title={expenses[3].title} 
+  amount={expenses[3].amount} 
+  date={expenses[3].date}
+  />
+  </>
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
     return (
         <div className="expense-item">
-      <div className="">Date</div>
-      <div className="expense-item__description"><h2>Title</h2><div className="expense-item__price">Amount</div></div>
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description"><h2>{props.title}</h2><div className="expense-item__price">{props.amount}</div></div>
         </div>
     )
 }
